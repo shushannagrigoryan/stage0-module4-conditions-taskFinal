a85c6b941535fd95de45167895fdd6e1c7ffc77b
Shushanna <shushannagrigoryan7@gmail.com>
2022-08-19 18:46:08 +0400

    3

--- src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/IntegerDivider.java
@@ -2,11 +2,8 @@ package school.mjc.stage0.conditions.finalTask;
 
 public class IntegerDivider {
     public void printCompletelyDivided(int dividend, int divider) {
-        System.out.println("divident" + dividend);
-        System.out.println("divider" + divider);
-        double d = (double)dividend / (double)divider;
-        System.out.println("d = " + d);
-        if(( d* (double)divider ) == (double) dividend){
+        int d = dividend / divider;
+        if( d* divider == dividend){
             System.out.println("can be divided completely");
 
         }
